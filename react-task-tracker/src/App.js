47428d8b955d68be6a90372ee297ded8779eb429
sutharketan <ketan@litmusblox.io>
2023-01-30 23:20:28 +0530

    add task feature

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,27 +1,30 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react"
+import AddTask from "./components/AddTask";
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
       text: 'Doctor appointment',
       day: 'Feb 5th at 2:30pm',
-      remider: true
+      reminder: true
     },
     {
       id: 2,
       text: 'School meeting',
       day: 'Feb 6th at 1:30pm',
-      remider: true
+      reminder: true
     },
     {
       id: 3,
       text: 'Food shopping',
       day: 'Feb 5th at 2:30pm',
-      remider: false
+      reminder: false
     }
   ])
 
@@ -34,13 +37,24 @@ function App() {
   // toggle reminder
   const toggleReminder = (id) => {
     console.log('toggle reminder: ' + id)
-    setTasks(tasks.map((task) => task.id === id ? {...task, remider: !task.remider} : task))
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
+  // add reminder
+  const addTask = (task) => {
+    console.log('task: ', task)
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+    console.log(tasks)
   }
 
   
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask) } 
+        showAdd={showAddTask}/>
+      { showAddTask && <AddTask onAdd={addTask} /> }
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} 
         onToggle={toggleReminder} />) : 'No tasks to show'}
     </div>
--- react-task-tracker/src/components/Task.js
+++ react-task-tracker/src/components/Task.js
@@ -2,7 +2,7 @@ import { FaTimes }  from 'react-icons/fa'
 
 function Task({task, onDelete, onToggle}) {
   return (
-    <div className={`task ${task.remider ? 'reminder':''}`} onDoubleClick={() => onToggle(task.id)}>
+    <div className={`task ${task.reminder ? 'reminder':''}`} onDoubleClick={() => onToggle(task.id)}>
         <h3>{task.text} <FaTimes onClick={() => onDelete(task.id)} /> </h3>
         <p>{task.day}</p>
     </div>
