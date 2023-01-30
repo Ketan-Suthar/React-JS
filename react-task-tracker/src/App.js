70eb4fd1d13a18ed02d65016f6b9b2be51243709
sutharketan <ketan@litmusblox.io>
2023-01-30 18:45:26 +0530

    add on delete functionality

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,40 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react"
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctor appointment',
+      day: 'Feb 5th at 2:30pm',
+      remider: true
+    },
+    {
+      id: 2,
+      text: 'School meeting',
+      day: 'Feb 6th at 1:30pm',
+      remider: true
+    },
+    {
+      id: 3,
+      text: 'Food shopping',
+      day: 'Feb 5th at 2:30pm',
+      remider: false
+    }
+  ])
+
+  // Delete task
+  const deleteTask = (id) => {
+    console.log('delete: ' + id)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+  
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : 'No tasks to show'}
     </div>
   );
 }
---
+++ react-task-tracker/src/components/Task.js
@@ -0,0 +1,12 @@
+import { FaTimes }  from 'react-icons/fa'
+
+function Task({task, onDelete}) {
+  return (
+    <div className="task">
+        <h3>{task.text} <FaTimes onClick={() => onDelete(task.id)}/> </h3>
+        <p>{task.day}</p>
+    </div>
+  )
+}
+
+export default Task
