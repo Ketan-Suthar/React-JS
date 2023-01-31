72d0df933621512683434d3aac230c043685e80a
sutharketan <ketan@litmusblox.io>
2023-01-31 18:05:50 +0530

    connect apis to server

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -6,44 +6,83 @@ import AddTask from "./components/AddTask";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
-
-  const [tasks, setTasks] = useState([
-  ])
+  const [tasks, setTasks] = useState([])
 
   useEffect(() => {
     const getTasks = async () => {
-      const tasks = await fetchTask()
-      setTasks(tasks)
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
 
     getTasks()
   }, [])
 
-  // fetch tasks
-  const fetchTask = async () => {
+  // Fetch Tasks
+  const fetchTasks = async () => {
     const res = await fetch('http://localhost:5000/tasks')
-    return await res.json()
+    const data = await res.json()
+
+    return data
   }
 
-  // Delete task
-  const deleteTask = (id) => {
-    console.log('delete: ' + id)
-    setTasks(tasks.filter((task) => task.id !== id))
+  // Fetch Task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
   }
 
-  // toggle reminder
-  const toggleReminder = (id) => {
-    console.log('toggle reminder: ' + id)
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  // Add Task
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
   }
 
-  // add reminder
-  const addTask = (task) => {
-    console.log('task: ', task)
-    const id = Math.floor(Math.random() * 1000) + 1
-    const newTask = {id, ...task}
-    setTasks([...tasks, newTask])
-    console.log(tasks)
+  // Delete Task
+  const deleteTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+    //We should control the response status to decide if we will change the state or not.
+    res.status === 200
+      ? setTasks(tasks.filter((task) => task.id !== id))
+      : alert('Error Deleting This Task')
+  }
+
+  // Toggle Reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    )
   }
 
   
